fc4950073401ea3422bb4c8d936e946c7e3d5550
Vidya-A-R <diovidya@gmail.com>
2024-01-12 16:57:51 +0700

    Garbage Collector

---
+++ Day_15_GBC/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+	static void Main()
+	{
+		string a = String.Empty;
+		int iteration = 10000;
+
+		Stopwatch stopwatch = new Stopwatch();
+		stopwatch.Start();
+		for (int i = 0; i < iteration; i++)
+		{
+			a += "Hello";
+			a += "World";
+		}
+		stopwatch.Stop();
+		Console.WriteLine(stopwatch.ElapsedMilliseconds);
+	}
+}
+
---
+++ Day_15_GBC_Hard/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        StringBuilder sb = new StringBuilder();
+        int iteration = 1_000_000_000;
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        for(int i = 0; i < iteration; i++)
+        {
+            sb.Append("a");
+            sb.Append("b");
+            sb.Replace('a', 'c');
+        }
+        stopwatch.Stop();
+
+        Console.WriteLine(stopwatch.ElapsedMilliseconds);
+    }
+}
