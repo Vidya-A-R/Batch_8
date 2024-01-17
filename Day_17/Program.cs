58b2c55028ca099b19603bcdb401e71800c45811
Vidya-A-R <diovidya@gmail.com>
2024-01-17 17:04:14 +0700

    Day_17

---
+++ Day_17/Program.cs
@@ -0,0 +1,21 @@
+﻿//#define JAJAL2
+#define JAJAL
+//#define JAJAL2
+class Program 
+{
+	static void Main()
+	{
+		#if CODE
+		Console.WriteLine("haha kamu mau kesini ya");
+		#elif JAJAL
+		Console.WriteLine("weloh kok sampek sini");
+		#elif JAJAL2
+		Console.WriteLine("akhire tekan kene wkwkwk");
+		#endif
+		Console.WriteLine("Rampung");
+	}
+}
+class Multiplier
+{
+	
+}
